9aca653b113397fdd1e5f5286da41deb7dea1d17
John Process <esologic@gmail.com>
2012-06-19 22:55:55 -0400

    foo

--- couch.lisp
+++ couch.lisp
@@ -12,13 +12,15 @@
      ,@body))
 
 (defmacro get-from-document (id name)
-  (with-gensyms (n doc)
+  (with-gensyms (n doc value)
     `(let* ((,n ,name)
-            (,doc (chillax:get-document *couch-db* ,id :errorp nil)))
-       (assoc (etypecase ,n
-                (symbol ,n)
-                (string (intern ,n)))
-              ,doc))))
+            (,doc (chillax:get-document *couch-db* ,id :errorp nil))
+            (,value (assoc (etypecase ,n
+                             (symbol ,n)
+                             (string (intern ,n)))
+                           ,doc)))
+       (if ,value
+           (read-from-string (cdr ,value))))))
 
 (defmacro set-in-document (id name value)
   (with-gensyms (n doc)
@@ -27,38 +29,18 @@
                   (symbol ,n)
                   (string (intern ,n))))
             (,doc (delete ,n (chillax:get-document *couch-db* ,id :errorp nil) :key #'car)))
-       (pushnew (cons ,n ,value) ,doc :key #'car)
+       (pushnew (cons ,n (format nil "~a" ,value)) ,doc :key #'car)
+       (format t "~a" ,doc)
        (chillax:put-document *couch-db* ,id ,doc))))
-
-(defmacro add-in-list-document (id name value)
-  (with-gensyms (n doc doc*)
-    `(let* ((,n ,name)
-            (,n (etypecase ,n
-                  (symbol ,n)
-                  (string (intern ,n))))
-            (,doc (chillax:get-document *couch-db* ,id :errorp nil))
-            (,doc* (assoc ,n ,doc)))
-       (if ,doc*
-           (rplacd ,doc* (cons ,value (cdr ,doc*)))
-           (push (cons ,n (list ,value)) ,doc))
-       (chillax:put-document *couch-db* ,id ,doc))))
-
-(defmacro remove-from-list-document (id name value)
-  (with-gensyms (n doc doc*)
-    `(let* ((,n ,name)
-            (,n (etypecase ,n
-                  (symbol ,n)
-                  (string (intern ,n))))
-            (,doc (chillax:get-document *couch-db* ,id :errorp nil))
-            (,doc* (assoc ,n ,doc)))
-       (when ,doc*
-         (rplacd ,doc* (delete ,value (cdr ,doc*) :test #'equal))
-         (chillax:put-document *couch-db* ,id ,doc)))))
 
 (defun set-login-time (id) (set-in-document id "last-login-time" (get-universal-time)))
 (defun get-login-time (id) (get-from-document id "last-login-time"))
 
 (defun add-discovery (id discovery)
-  (add-in-list-document id "discoveries" discovery))
+  (let ((discoveries (get-from-document id "discoveries")))
+    (set-in-document id "discoveries" (cons discovery discoveries))))
 (defun remove-discovery (id discovery)
-  (remove-from-list-document id "discoveries" discovery))
+  (let ((discoveries (get-from-document id "discoveries")))
+    (set-in-document id "discoveries" (delete discovery discoveries :test #'equal))))
+(defun get-discoveries (id)
+  (get-from-document id "discoveries"))
