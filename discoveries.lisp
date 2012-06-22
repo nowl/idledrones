bbfe2cb722beed9cadc1ec48105caaa8a07056fa
John Process <esologic@gmail.com>
2012-06-22 00:00:46 -0400

    discovery properties

--- discoveries.lisp
+++ discoveries.lisp
@@ -1,18 +1,40 @@
 (in-package :idrones-sys)
 
-(defparameter *discovery-types*
+(define-choice-weights *discovery-types*
   '(("system" . 5)
     ("planet" . 20)
     ("asteroid" . 1)
     ("alien craft" . 50)
-    ("alien planet" . 100)))    
+    ("alien planet" . 100)))
 
+;; (chance of appearing) (extraction potential)
+(defparameter *resource-table*
+  '(("asteroid" . (("light ores"  0.75 0.5)
+                   ("heavy ores"  0.1  0.1)
+                   ("light gases" 0.3  0.25)))
+    ("system" . (("light ores"  1.0 0.75)
+                 ("heavy ores"  0.75  0.5)
+                 ("rare ores"  0.1  0.1)
+                 ("light gases" 0.7  0.45)
+                 ("heavy gases" 0.4  0.25)
+                 ("rare gases" 0.2  0.15)))))
+  
 (defun possibly-make-discovery (prob-of-discovery num-exp-drones num-discoveries)
   (let ((chance-of-disc (min (max (/ (* prob-of-discovery num-exp-drones) num-discoveries)
                                   prob-of-discovery)
                              1.0)))
     (unless (check-roll chance-of-disc)
       (return-from possibly-make-discovery nil))
-    (list :name (make-name)
-          :type (
+    (let ((type (random-choice *discovery-types*)))
+      (list :name (make-name)
+            :type type
+            :resources (remove-if #'null
+                                  (loop for resource in (cdr (assoc type *resource-table* :test #'equal)) collecting
+                                       (destructuring-bind (res-type appear-prob extract-prob) resource
+                                         (if (check-roll appear-prob)
+                                             (cons res-type (random extract-prob))))))))))
+            
+                             
+                         
+          
     
--- utils.lisp
+++ utils.lisp
@@ -7,17 +7,47 @@
 (defun check-roll (prob)
   (<= (random 1.0) prob))
 
-(defun make-cdf-from-choices/weights (choices)
-  (let* ((total (loop for choice in choices summing (cdr choice)))
-         (inv-totals (loop for choice in choices collecting (cons (car choice)
-                                                                  (- total (cdr choice)))))
-         (inv-totals-sum (loop for choice in inv-totals summing (cdr choice)))
-         (probs (loop for choice in inv-totals collecting (cons (car choice)
-                                                                (/ (float (cdr choice)) inv-totals-sum)))))
-    inv-totals))
-    
+;; choice stuff
 
-(defun choice-with-weights (choices)
-  "choices should be of the format ((choice1 . weight1) (choice2
-. weight2) etc.)"
+(defstruct (choices)
+  (weights)
+  (cdf))
+
+(defun make-pdf-from-choices/weights (choices)
+  (let ((factors (cdr (loop with prev for c in choices collecting
+                           (prog1
+                               (when prev
+                                 (/ (cdr c) (float prev)))
+                             (setf prev (cdr c)))))))
+    (labels ((rec (factors weight)
+               (if (null factors)
+                   (list (/ 1.0 weight))
+                   (let ((pn (rec (cdr factors) (1+ (* weight (car factors))))))
+                     (cons (* (car factors) (car pn)) pn)))))
+      (let ((probs (rec factors 1)))
+        (loop for c in choices for p in probs collecting
+             (cons (car c) p))))))
+
+(defun make-cdf-from-pdf (pdf)
+  (let* ((sorted (sort pdf #'> :key #'cdr)))
+    (let ((cdf-total 0))
+      (loop for v in sorted collect
+           (progn
+             (incf cdf-total (cdr v))
+             (cons (car v) cdf-total))))))
+
+(defun random-choice (choices)
+  (declare (choices choices))
+  (let ((choice (random 1.0)))
+    (loop for c in (choices-cdf choices) do
+         (when (<= choice (cdr c))
+           (return (car c))))))
+
+(defmacro define-choice-weights (types-var choices)
+  (let ((cs (gensym "choices")))
+    `(let ((,cs ,choices))
+       (defparameter ,types-var
+         (make-choices :weights ,cs
+                       :cdf (make-cdf-from-pdf 
+                             (make-pdf-from-choices/weights ,cs)))))))
   
