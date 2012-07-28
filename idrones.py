8a0024fe57a2661a5c9f32faafd07d1767e5ed8f
John Process <esologic@gmail.com>
2012-07-28 00:56:15 -0400

    fail safe

--- idrones.py
+++ idrones.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
+from mongo import MongoInterface
+from functools import wraps
 import urllib, urllib2, json
     
 app = Flask(__name__)
@@ -7,10 +9,19 @@ app = Flask(__name__)
 BROWSER_ID_URL = "https://browserid.org/verify"
 LOCAL_SERVER = "http://localhost:5000"
 
+@app.before_request
+def before_request():
+    g.db = MongoInterface()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
 @app.route("/")
 def show_entries():
     email = session.get('email', None)
-    return render_template('index.html', id=email)
+    last_login = g.db.get_login_time(email)
+    return render_template('index.html', id=email, last_login=last_login)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -20,6 +31,7 @@ def login():
     #app.logger.debug(str(result))
     if result['status'] == 'okay' and result['audience'] == LOCAL_SERVER:
         session['email'] = result['email']
+        g.db.set_login_time(session['email'])
         flash('You were logged in')
     return redirect(url_for('show_entries'))
 
@@ -28,6 +40,21 @@ def logout():
     session.pop('email', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwds):
+        if session.get('email', None) == None:
+            return redirect(url_for('show_entries'))
+        return f(*args, **kwds)
+    return decorated_function
+
+@app.route('/discoveries')
+@login_required
+def discoveries():
+    email = session.get('email', None)
+    ds = g.db.get_discoveries(email)
+    return render_template('discoveries.html', id=email, discoveries=ds)
 
 if __name__ == '__main__':
     app.config.from_pyfile('idrones_webapp.cfg')
--- mongo.py
+++ mongo.py
@@ -1,6 +1,18 @@
 from pymongo import Connection
 import datetime
+from functools import wraps
         
+def fail_safe(typ):
+    def fail_safe_f(f):
+        @wraps(f)
+        def decorator(*args, **kwds):
+            try:
+                return f(*args, **kwds)
+            except:
+                return typ
+        return decorator
+    return fail_safe_f
+
 class MongoInterface ():
     def __init__(self,
                  server = 'localhost',
@@ -10,6 +22,12 @@ class MongoInterface ():
         self.connection = Connection(server, port)
         self.db = self.connection[db_name]
         self.collection = self.db[collection]
+
+    def close(self):
+        try:
+            self.connection.close()
+        except:
+            pass
     
     def _get(self, spec):
         return self.collection.find_one(spec)
@@ -24,15 +42,31 @@ class MongoInterface ():
 
     def set_login_time(self, user):        
         self._set({'user': user,
-                   'type': 'login-time'},
+                   'type': 'user_info'},
                   'last-login-time',
                   datetime.datetime.utcnow())
     
+    @fail_safe(None)
     def get_login_time(self, user):
-        return self._get({'user': user, 'type': 'login-time'})
+        return self._get({'user': user, 'type': 'user_info'})['last-login-time']
+
+    def set_discoveries(self, user, discoveries):
+        self._set({'user': user,
+                   'type': 'user_info'},
+                  'discoveries',
+                  repr(discoveries))
+
+    @fail_safe([])
+    def get_discoveries(self, user):
+        return eval(self._get({'user': user, 'type': 'user_info'})['discoveries'])
 
 
 if __name__ == '__main__':
     mint = MongoInterface()
-    mint.set_login_time('firestaff@gmail.com')
-    print mint.get_login_time('firestaff@gmail.com')
+    #mint.set_login_time('firestaff@gmail.com')
+    #print mint.get_login_time('firestaff@gmail.com')
+    #mint.get_discoveries('firestaff@gmail.com')
+    #mint.set_discoveries('firestaff@gmail.com',
+    #                     [{'name': 'name 1', 'type': 'asteroid'},
+    #                      {'name': 'name 2', 'type': 'system'},
+    #                      {'name': 'name 3', 'type': 'alien craft'},])
